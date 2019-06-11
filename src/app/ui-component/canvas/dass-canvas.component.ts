acf0226703322e0be9fdafe1269b75975b0bfc25
권진오 <kjo133@taehwainno.com>
2019-06-11 13:43:41 +0900

    캔버스 복구

--- src/app/ui-component/canvas/dass-canvas.component.ts
+++ src/app/ui-component/canvas/dass-canvas.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angula
 })
 export class DassCanvasComponent implements AfterViewInit, OnInit {
   /**
-   * 뷰 요소 캔버스 객체
-   * 
-   * @description DOM 요소에 있는 타입과 캔버스 객체를 컨버팅하는 과정이다.
-   */
+     * 뷰 요소 캔버스 객체
+     * 
+     * @description DOM 요소에 있는 타입과 캔버스 객체를 컨버팅하는 과정이다.
+     */
   @ViewChild('baseCanvasEl') baseCanvasEl: ElementRef;
 
   /**
@@ -41,25 +41,25 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
   scaleCtx: CanvasRenderingContext2D;
 
   /**
-   * 기본 사각형값
-   * 
-   * @property x 가로
-   * @property y 세로
-   * @property w 이동한 가로
-   * @property h 이동한 세로
-   */
+ * 기본 사각형값
+ * 
+ * @property x 가로
+ * @property y 세로
+ * @property w 이동한 가로
+ * @property h 이동한 세로
+ */
   baseRect: {
     x: number,
     y: number,
     w: number,
     h: number
   } = {
-    x: 75,
-    y: 256,
-    w: 265,
-    h: 99
-  }
- 
+      x: 75,
+      y: 256,
+      w: 265,
+      h: 99
+    }
+
   /**
    * 각도 값
    */
@@ -78,12 +78,11 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
   /**
    * 이미지 주소
    */
-  // imgAddr: string = this.img.src = 'http://www.mhc.kr/files/attach/images/779/229/882/006/a9428b7f2adb9b5b243255d9c5c491dd.jpg';
-  imgAddr: string = this.img.src = 'http://d3qpgbf7vej5yf.cloudfront.net/wp-content/uploads/2016/04/INUPATHY-01212.jpg';
+  imgAddr: string = this.img.src = 'http://www.mhc.kr/files/attach/images/779/229/882/006/a9428b7f2adb9b5b243255d9c5c491dd.jpg';
 
   /**
-   * 생성자이다.
-   */
+ * 생성자이다.
+ */
   constructor() { }
 
   /**
@@ -116,7 +115,7 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
    * 화면 부르기전 초기화 함수다.
    */
   ngOnInit() {
-    
+
   }
 
   /**
@@ -149,18 +148,15 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
    * @param h 이동한 높이값
    */
   drawScale(x: number, y: number, w: number, h: number) {
-    // 라디안이 180도 라면... 
-    // TODO 90도 270도일때도 값 구하기
-    // 좌표 계산값 다시 생각해보기...
     if (this.radian === 180) {
-        x = this.img.width - x;
-        y = this.img.height - y;
-        w = -w;
-        h = -h;
+      x = this.img.width - x;
+      y = this.img.height - y;
+      w = -w;
+      h = -h;
     }
 
     this.scaleCtx.clearRect(0, 0, this.scaleCanvas.width, this.scaleCanvas.height);
-    
+
     // 현재 값을 저장한다.
     this.scaleCtx.save();
 
@@ -205,8 +201,8 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
   }
 
   /***********************************************************************************************
-  *                                      Helper Objects                                         *
-  ***********************************************************************************************/
+   *                                      Helper Objects                                         *
+   ***********************************************************************************************/
 
   /**
    * 마우스 좌표를 가져온다.
@@ -215,8 +211,8 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
    */
   getMousePos(evt: MouseEvent) {
     return {
-        x: evt.clientX - this.baseCanvas.getBoundingClientRect().left,
-        y: evt.clientY - this.baseCanvas.getBoundingClientRect().top
+      x: evt.clientX - this.baseCanvas.getBoundingClientRect().left,
+      y: evt.clientY - this.baseCanvas.getBoundingClientRect().top
     };
   }
 
@@ -230,10 +226,10 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
    */
   getScaleRangeByRect(x: number, y: number, w: number, h: number) {
     return {
-        x: x / this.baseCanvas.width * this.img.width,
-        y: y / this.baseCanvas.height * this.img.height,
-        w: y / this.baseCanvas.width * this.img.width,
-        h: h / this.baseCanvas.height * this.img.height,
+      x: x / this.baseCanvas.width * this.img.width,
+      y: y / this.baseCanvas.height * this.img.height,
+      w: w / this.baseCanvas.width * this.img.width,
+      h: h / this.baseCanvas.height * this.img.height,
     }
   }
 
@@ -244,33 +240,33 @@ export class DassCanvasComponent implements AfterViewInit, OnInit {
     // 콜백으로 내가 원하는 함수로 바인딩 되지 않음... 콜백 함수 구현체에다가 내가 만든 함수를 호출
     // 1. 캔버스 마우스진입해서 마우스 오버로 좌표를 체크한다.
     this.baseCanvas.addEventListener('mousedown', (evt) => {
-        this.isMouseDown = true;
+      this.isMouseDown = true;
 
-        const coordinate = this.getMousePos(evt);
-        // 전체 가로 - 캔버스 왼쪽값 / 캔버스 오른쪽값 - 캔버스 왼쪽값 * 캔버스 가로
-        this.baseRect.x = coordinate.x;
-        this.baseRect.y = coordinate.y;
+      const coordinate = this.getMousePos(evt);
+      // 전체 가로 - 캔버스 왼쪽값 / 캔버스 오른쪽값 - 캔버스 왼쪽값 * 캔버스 가로
+      this.baseRect.x = coordinate.x;
+      this.baseRect.y = coordinate.y;
     });
 
     // 2. 마우스가 클릭이 시작되었을때 해당 좌표를 구하며 있다가 시작을 하였을때 좌표를 구한다.
     this.baseCanvas.addEventListener('mousemove', (evt) => {
-        // 마우스 클릭상태가 아니라면...
-        if (!this.isMouseDown) {
-            return;
-        }
+      // 마우스 클릭상태가 아니라면...
+      if (!this.isMouseDown) {
+        return;
+      }
 
-        const coordinate = this.getMousePos(evt);
-        this.baseRect.w = coordinate.x - this.baseRect.x;
-        this.baseRect.h = coordinate.y - this.baseRect.y;
+      const coordinate = this.getMousePos(evt);
+      this.baseRect.w = coordinate.x - this.baseRect.x;
+      this.baseRect.h = coordinate.y - this.baseRect.y;
 
-        this.drawBase();
+      this.drawBase();
 
-        let scaleRect = this.getScaleRangeByRect(this.baseRect.x, this.baseRect.y, this.baseRect.w, this.baseRect.h);
-        this.drawScale(scaleRect.x, scaleRect.y, scaleRect.w, scaleRect.h);
+      let scaleRect = this.getScaleRangeByRect(this.baseRect.x, this.baseRect.y, this.baseRect.w, this.baseRect.h);
+      this.drawScale(scaleRect.x, scaleRect.y, scaleRect.w, scaleRect.h);
 
-        this.drawRectangle(this.baseRect.x, this.baseRect.y, this.baseRect.w, this.baseRect.h);
+      this.drawRectangle(this.baseRect.x, this.baseRect.y, this.baseRect.w, this.baseRect.h);
 
-        document.getElementById('x').innerHTML =
+      document.getElementById('x').innerHTML =
         `
             sx: ${this.baseRect.x} <br/>
             sy: ${this.baseRect.y} <br/>
